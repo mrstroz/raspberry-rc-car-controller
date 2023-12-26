1731cc2611f98eac3ab350909762cae7a3ba030c
Mariusz Stróż <manieczny86@gmail.com>
2023-12-26 22:16:11 +0100

    Add GPIO implementation

--- components/motor.py
+++ components/motor.py
@@ -1,22 +1,24 @@
-class Motor:
+import RPi.GPIO as GPIO
+
+class Motor():
     def __init__(self, ena, input1, input2):
         self.ena = ena
         self.input1 = input1
         self.input2 = input2
-        # GPIO.setup(self.ena, GPIO.OUT)
-        # GPIO.setup(self.input1,GPIO.OUT)
-        # GPIO.setup(self.input2,GPIO.OUT)
-        # self.pwm = GPIO.PWM(self.ena,100)
-        # self.pwm.start(0)
+        GPIO.setup(self.ena, GPIO.OUT)
+        GPIO.setup(self.input1,GPIO.OUT)
+        GPIO.setup(self.input2,GPIO.OUT)
+        self.pwm = GPIO.PWM(self.ena,100)
+        self.pwm.start(0)
 
     def forward(self, speed):
         print('Move Forward: ' + str(speed))
-        # GPIO.output(self.input1, GPIO.LOW)
-        # GPIO.output(self.input2, GPIO.HIGH)
-        # self.pwm.ChangeDutyCycle(x)
+        GPIO.output(self.input1, GPIO.LOW)
+        GPIO.output(self.input2, GPIO.HIGH)
+        self.pwm.ChangeDutyCycle(speed)
 
     def backward(self, speed):
         print('Move Backward: ' + str(speed))
-        # GPIO.output(self.input1, GPIO.HIGH)
-        # GPIO.output(self.input2, GPIO.LOW)
-        # self.pwm.ChangeDutyCycle(x)
+        GPIO.output(self.input1, GPIO.HIGH)
+        GPIO.output(self.input2, GPIO.LOW)
+        self.pwm.ChangeDutyCycle(speed)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import RPi.GPIO as GPIO
 
 from dotenv import load_dotenv
 from flask import Flask, render_template
@@ -10,20 +11,15 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 socketio = SocketIO(app)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
+speed_left = 50
+speed_right = 50
+motor_left = Motor(2, 3, 4)
+motor_right = Motor(14, 15, 18)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@socketio.on('send_controller_data')
-def handle_stick_data(json):
-    motor_left = Motor(2, 3, 4)
-    motor_right = Motor(2, 3, 4)
-    speed_left = int(json['left'])
-    speed_right = int(json['left'])
-
+while True:
     if speed_left > 0:
         motor_left.forward(abs(speed_left))
     else:
@@ -34,6 +30,19 @@ def handle_stick_data(json):
     else:
         motor_right.backward(abs(speed_right))
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('send_controller_data')
+def handle_stick_data(json):
+    global speed_left, speed_right
+    
+    speed_left = int(json['left'])
+    speed_right = int(json['left'])
+
+    
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
